de6a2cd1152c2985777ab211cfac125482ef310b
FarrowLS <peterwfarrow@yahoo.com>
2014-04-03 16:47:47 -0400

    Updated db settings

--- pure_assessment_app/settings/base.py
+++ pure_assessment_app/settings/base.py
@@ -72,12 +72,12 @@ WSGI_APPLICATION = 'pure_assessment_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/

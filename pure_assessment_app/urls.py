50777f6fc16cafc26dbade2678729372ddfe8636
FarrowLS <peterwfarrow@yahoo.com>
2014-04-08 14:28:23 -0400

    Updated main urls.py to point homepage into an app

--- pure_assessment_app/urls.py
+++ pure_assessment_app/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from django.http import HttpResponseRedirect
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +11,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^textpage/', include('textpage.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    # For redirecting root url
+    (r'^$', lambda r : HttpResponseRedirect('textpage/')),
 )

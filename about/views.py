80c0dda547e02ee11bd92de5efa7c3020418b54a
FarrowLS <peterwfarrow@yahoo.com>
2014-04-10 14:44:11 -0400

    Bringing the README text into the /about page

--- about/views.py
+++ about/views.py
@@ -1,11 +1,15 @@
 # from django.http import HttpResponse
 # from django.template import RequestContext, loader
 
+import re
+
 from django.shortcuts import render
 
+raw_about_text = open("README", "r")
+about_text = raw_about_text.read()
 
 def index(request):
-    context = {'text1': 'Hi!!!', 'text2': 'Bye!!!',}
+    context = {'text1': 'Hi!!!', 'text2': about_text,}
     return render(request, 'about/index.html', context)    
     # return render(request, 'base.html', context)
     # return HttpResponse('Hi!')

8a242d7ffc47bb1e1ed46d3844c675830c8e54bd
FarrowLS <peterwfarrow@yahoo.com>
2014-04-11 15:26:10 -0400

    Removed model for first Heroku South deployment

--- about/models.py
+++ about/models.py
@@ -1,6 +1,6 @@
-from django.db import models
-
-class Page(models.Model):
-    title = models.CharField(max_length=500)
-    body = models.TextField()
-    pub_date = models.DateTimeField('date published')
+# from django.db import models
+#
+# class Page(models.Model):
+#     title = models.CharField(max_length=500)
+#     body = models.TextField()
+#     pub_date = models.DateTimeField('date published')

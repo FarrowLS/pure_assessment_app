dfb36f6939b42004c93dfafbb19adc30d26a443f
FarrowLS <peterwfarrow@yahoo.com>
2014-04-12 14:01:50 -0400

    Added subtitle field to about model and create a migration for it

---
+++ about/migrations/0002_auto__add_field_page_subtitle.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Page.subtitle'
+        db.add_column(u'about_page', 'subtitle',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=500),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Page.subtitle'
+        db.delete_column(u'about_page', 'subtitle')
+
+
+    models = {
+        u'about.page': {
+            'Meta': {'object_name': 'Page'},
+            'body': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'subtitle': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '500'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '500'})
+        }
+    }
+
+    complete_apps = ['about']

b36888b89f5c36a23d383e33789a845d3be7b710
FarrowLS <peterwfarrow@yahoo.com>
2014-04-12 16:35:43 -0400

    Removed some testing text

--- about/views.py
+++ about/views.py
@@ -9,7 +9,7 @@ raw_about_text = open("README", "r")
 about_text = raw_about_text.read()
 
 def index(request):
-    context = {'text1': '[placeholder]', 'text2': about_text,}
+    context = {'text2': about_text,}
     return render(request, 'about/index.html', context)    
     # return render(request, 'base.html', context)
     # return HttpResponse('Hi!')

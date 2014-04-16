5303fc5b89b3caf8bf044838f0568c5be1859d93
FarrowLS <peterwfarrow@yahoo.com>
2014-04-16 14:28:31 -0400

    Added django-admin-honeypot and removed old settings directory

--- pure_assessment_app/urls.py
+++ pure_assessment_app/urls.py
@@ -10,7 +10,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'pure_assessment_app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^about/', include('about.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include('admin_honeypot.urls')),
+    url(r'^notanormaladminpath/', include(admin.site.urls)),
     # For redirecting root url
     (r'^$', lambda r : HttpResponseRedirect('about/')),
 )

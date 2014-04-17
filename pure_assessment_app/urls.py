f91fcd5d815f31ca89de7c5b1541a8ef174bbbf9
FarrowLS <peterwfarrow@yahoo.com>
2014-04-17 15:30:25 -0400

    Adding access control experiments

--- pure_assessment_app/urls.py
+++ pure_assessment_app/urls.py
@@ -9,6 +9,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pure_assessment_app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    # My paths
+    # url(r'^logout/', include('')),
     url(r'^about/', include('about.urls')),
     url(r'^admin/', include('admin_honeypot.urls')),
     url(r'^notanormaladminpath/', include(admin.site.urls)),

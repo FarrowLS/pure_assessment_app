7f2bf0798def1ebcbf66d2067e4dbc720ba8031c
FarrowLS <peterwfarrow@yahoo.com>
2014-04-29 17:36:24 -0400

    Added alternative settings to test running on a non-Heroku environment

---
+++ pure_assessment_app/alt_settings.py
@@ -0,0 +1,28 @@
+# settings/development.py
+
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pure_assessment_app_alt_db',
+        'USER': 'peterfarrow',
+        'PASSWORD': os.environ["DEV_DATABASE_PASSWORD"],
+        'HOST': '',
+        'PORT': '',
+    }
+}

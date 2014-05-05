503cd6d6ef6d6ad181f9ac766aab932ba9ecf0a1
FarrowLS <peterwfarrow@yahoo.com>
2014-05-05 15:35:41 -0400

    Updated navigation

--- assessment/views.py
+++ assessment/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 def index(request):
-    indextext = "Your tests"
+    indextext = "Your tests will go here!"
     context = {'text1': indextext,}  
     return render(request, 'assessment/index.html', context)
     # return HttpResponse("You are at assessment index!")

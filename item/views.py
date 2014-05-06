081fcf4afca4326c84010e262a5976563508f3f9
FarrowLS <peterwfarrow@yahoo.com>
2014-05-06 16:04:41 -0400

    Updated Item index view and About index links

--- item/views.py
+++ item/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
+
+# from item.models 
 
 def index(request):
-    return HttpResponse("You're here on the items index page!") 
+    # return HttpResponse("You're here on the items index page!")
+    index_text = "The list of item banks goes here"
+    context = {'bodytext': index_text,}
+    return render(request, 'item/index.html', context) 

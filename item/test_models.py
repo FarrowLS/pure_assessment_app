04359ca4ef685e2faa327749840a0c43028e2b69
FarrowLS <peterwfarrow@yahoo.com>
2014-05-07 15:58:06 -0400

    Updated test_models.py for Item

--- item/test_models.py
+++ item/test_models.py
@@ -1,1 +1,10 @@
-# Tests for Item models
+from django.test import TestCase
+
+from items.models import Itembank
+
+def create_itembank(name):
+    """
+    Creates an itembank with a title
+    """
+    return Itembank.objects.create(name=name)
+

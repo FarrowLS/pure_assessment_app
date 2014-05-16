5d88c9e0171a173ecdafcde1274b93a62768800a
FarrowLS <peterwfarrow@yahoo.com>
2014-05-16 16:46:22 -0400

    Added models and admin file to assessment module

---
+++ assessment/models.py
@@ -0,0 +1,15 @@
+# assessment/models.py
+
+from django.db import models
+# IMPORT USER INFO
+
+from itembank.models import Itembank
+
+"""
+class Assessment(models.Model):
+    models.ForeignKey(Itembank)
+    # USER RELATIONSHIP
+    # NUMBER OF ITEMS
+    # NUMBER OF CORRECT ITEMS NEEDED TO PASS
+
+"""

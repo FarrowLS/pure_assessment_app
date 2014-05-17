695dfd559a304ab00eca32efb2d43f25807081ea
FarrowLS <peterwfarrow@yahoo.com>
2014-05-17 16:13:29 -0400

    Work in progress for Assessment class

--- assessment/models.py
+++ assessment/models.py
@@ -1,15 +1,16 @@
 # assessment/models.py
 
 from django.db import models
-# IMPORT USER INFO
+# IMPORT USER INFO from django.db import models
 
 from itembank.models import Itembank
 
 """
 class Assessment(models.Model):
-    models.ForeignKey(Itembank)
-    # USER RELATIONSHIP
+    itembank = models.ForeignKey(Itembank)
+    # SHOULD I USE THIS OR USE django-model-utils INSTEAD? created = models.DateTimeField(auto_now_add=True)
+    # USER RELATIONSHIP testee = models.[?]ForeignKey[?](settings.AUTH_USER_MODEL)
     # NUMBER OF ITEMS
     # NUMBER OF CORRECT ITEMS NEEDED TO PASS
-
 """
+

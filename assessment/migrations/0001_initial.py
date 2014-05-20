484e57560cb80cf6e945a997c8adeaaf1a124489
FarrowLS <peterwfarrow@yahoo.com>
2014-05-20 13:31:00 -0400

    Started rebuilding Assessment class

--- assessment/migrations/0001_initial.py
+++ assessment/migrations/0001_initial.py
@@ -13,7 +13,9 @@ class Migration(SchemaMigration):
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('created', self.gf('model_utils.fields.AutoCreatedField')(default=datetime.datetime.now)),
             ('modified', self.gf('model_utils.fields.AutoLastModifiedField')(default=datetime.datetime.now)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
             ('itemsneeded', self.gf('django.db.models.fields.IntegerField')(default=1)),
+            ('itemsneededtopass', self.gf('django.db.models.fields.IntegerField')(default=1)),
         ))
         db.send_create_signal(u'assessment', ['Assessment'])
 
@@ -29,7 +31,9 @@ class Migration(SchemaMigration):
             'created': ('model_utils.fields.AutoCreatedField', [], {'default': 'datetime.datetime.now'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'itemsneeded': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
-            'modified': ('model_utils.fields.AutoLastModifiedField', [], {'default': 'datetime.datetime.now'})
+            'itemsneededtopass': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'modified': ('model_utils.fields.AutoLastModifiedField', [], {'default': 'datetime.datetime.now'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
         }
     }
 
--- assessment/models.py
+++ assessment/models.py
@@ -1,19 +1,20 @@
 # from django.conf import settings
 from django.db import models
 
-from model_utils.models import TimeStampedModel, StatusModel
+from model_utils.models import TimeStampedModel
+from model_utils.fields import StatusField
 from model_utils import Choices 
 
 from itembank.models import Itembank
 
-# REMOVE STATUSMODEL
-class Assessment(TimeStampedModel, StatusModel):
-    itembank = models.ForeignKey(Itembank)
+class Assessment(TimeStampedModel):
+    # itembank = models.ForeignKey(Itembank)
+    name = models.CharField(max_length=200)
     itemsneeded = models.IntegerField('number of items in assessment', default = 1) 
     itemsneededtopass = models.IntegerField('number of items needed to pass the assessment', default = 1)
-    STATUS = Choices('active', 'inactive')
-    # def __unicode__(self):
-    #     return self.name
+    # STATUS = Choices('active', 'inactive')
+    def __unicode__(self):
+        return self.name
 
 '''
 class Assessment(TimeStampedModel, StatusModel):

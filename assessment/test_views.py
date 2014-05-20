ee5e4256aeb6b7ec7c285cfbe3e97d241f87b6c5
FarrowLS <peterwfarrow@yahoo.com>
2014-05-20 15:54:20 -0400

    Added list of active assessments to /assessments

--- assessment/test_views.py
+++ assessment/test_views.py
@@ -3,7 +3,20 @@ from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User
 
-# ADD TEST CREATION HERE LATER
+from itembank.models import Itembank
+from assessment.models import Assessment
+
+def create_itembank(name):
+    """
+    Creates an itembank with a name
+    """
+    return Itembank.objects.create(name=name)
+
+def create_assessment(name, itembank):
+    """
+    Creates a test with a name and relationship to itembank
+    """
+    return Assessment.objects.create(name=name, itembank=itembank)
 
 class AssessmentIndexTests(TestCase):
     def test_index_can_not_be_seen_by_anonymous_user(self):
@@ -16,11 +29,13 @@ class AssessmentIndexTests(TestCase):
 
     def test_index_exists(self):
         """
-        The Assessment index page should be at /assessment
+        The Assessment index page should be at /assessment and should have a list of active assessment
         """
+        test_itembank1 = create_itembank(name="Itembank1")
+        test_assessment1 = create_assessment(name="Test1", itembank=test_itembank1)
         test_user_setup = User.objects.create_user(username='bob', password='secret')
         test_user = Client()
         test_user.login(username='bob', password='secret')  
         response = test_user.get(reverse('assessmentindex'), **{'wsgi.url_scheme': 'https'})
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Your tests")
+        self.assertContains(response, "Test1")
--- assessment/views.py
+++ assessment/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
+from assessment.models import Assessment
+
 def index(request):
-    indextext = '[some test text: Your tests]'
-    context = {'text1': indextext,}  
+    active_assesments_list = Assessment.objects.order_by('modified')
+    context = {'active_assesments_list': active_assesments_list,}  
     return render(request, 'assessment/index.html', context)
     # return HttpResponse("You are at assessment index!")

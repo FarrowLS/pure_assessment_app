28a44c2a15ada3c237f6dee55044f412942c414b
FarrowLS <peterwfarrow@yahoo.com>
2014-05-20 16:39:35 -0400

    Added list of inactive assessments to /assessments

--- assessment/test_views.py
+++ assessment/test_views.py
@@ -26,10 +26,20 @@ class AssessmentIndexTests(TestCase):
         response = self.client.get(reverse('assessmentindex'), **{'wsgi.url_scheme': 'https'})
         self.assertEqual(response.status_code, 302)
 
-
-    def test_index_exists(self):
+    def test_no_active_assesments(self):
         """
-        The Assessment index page should be at /assessment and should have a list of active assessment
+        The Assessment page should indicate there are no assessments to complete
+        """
+        test_user_setup = User.objects.create_user(username='bob', password='secret')
+        test_user = Client()
+        test_user.login(username='bob', password='secret')
+        response = test_user.get(reverse('assessmentindex'), **{'wsgi.url_scheme': 'https'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "You do not have any tests to complete at this time.")
+
+    def test_active_assesments(self):
+        """
+        The assessment index page should have a list of active assessments when they exist
         """
         test_itembank1 = create_itembank(name="Itembank1")
         test_assessment1 = create_assessment(name="Test1", itembank=test_itembank1)
@@ -39,3 +49,18 @@ class AssessmentIndexTests(TestCase):
         response = test_user.get(reverse('assessmentindex'), **{'wsgi.url_scheme': 'https'})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test1")
+
+    def test_no_completed_assesments(self):
+        """
+        The Assessment page should indicate there are no completed assessments
+        """
+        test_user_setup = User.objects.create_user(username='bob', password='secret')
+        test_user = Client()
+        test_user.login(username='bob', password='secret')
+        response = test_user.get(reverse('assessmentindex'), **{'wsgi.url_scheme': 'https'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "You have not completed any tests yet.")
+
+    # TO BE ADDED def test_completed_assesments(self): 
+
+ 
--- assessment/views.py
+++ assessment/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import get_object_or_404, render
 from assessment.models import Assessment
 
 def index(request):
-    active_assesments_list = Assessment.objects.order_by('modified')
-    context = {'active_assesments_list': active_assesments_list,}  
+    active_assesments_list = Assessment.objects.all().exclude(status='inactive')
+    finished_assesments_list = Assessment.objects.all().filter(status='inactive')
+    context = {'active_assesments_list': active_assesments_list, 
+               'finished_assesments_list': finished_assesments_list,}  
     return render(request, 'assessment/index.html', context)
     # return HttpResponse("You are at assessment index!")

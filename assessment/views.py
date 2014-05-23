a2a72c1cea8a1560cd0add397377a26faeb5f5bc
FarrowLS <peterwfarrow@yahoo.com>
2014-05-23 00:27:34 -0400

    Updated view to handle new TesteeAssessment

--- assessment/views.py
+++ assessment/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
-from assessment.models import Assessment
+from assessment.models import TesteeAssessment
 
 def index(request):
-    active_assesments_list = Assessment.objects.all().exclude(status='inactive') # TO BE UPDATED TO NEW STATUSES
-    finished_assesments_list = Assessment.objects.all().filter(status='inactive') # TO BE UPDATED TO NEW STATUSES
+    active_assesments_list = TesteeAssessment.objects.all().exclude(status='passed').exclude(status='failed') 
+    finished_assesments_list = TesteeAssessment.objects.all().exclude(status='not-started').exclude(status='started') 
     context = {'active_assesments_list': active_assesments_list, 
                'finished_assesments_list': finished_assesments_list,}  
     return render(request, 'assessment/index.html', context)

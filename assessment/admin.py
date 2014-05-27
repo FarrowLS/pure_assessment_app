a8cc7cfd6003c9b1b50337ead7af6ee57f8d9dd9
FarrowLS <peterwfarrow@yahoo.com>
2014-05-27 16:24:02 -0400

    Updated admin for TesteeResponse

--- assessment/admin.py
+++ assessment/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
-from assessment.models import Assessment, TesteeAssessment
+from assessment.models import Assessment, TesteeAssessment, TesteeResponse
+
+class TesteeResponseInline(admin.StackedInline):
+    model = TesteeResponse
+    extra = 0
+
+class TesteeAssessmentAdmin(admin.ModelAdmin):
+    inlines = [TesteeResponseInline] 
 
 admin.site.register(Assessment)
-admin.site.register(TesteeAssessment)
-
+admin.site.register(TesteeAssessment, TesteeAssessmentAdmin)
+# admin.site.register(TesteeResponse)

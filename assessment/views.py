69dfd02c4f4be938bbe535eb634e83b3ff8459d8
FarrowLS <peterwfarrow@yahoo.com>
2014-05-29 12:28:35 -0400

    filtered /assessment view by user

--- assessment/views.py
+++ assessment/views.py
@@ -4,13 +4,15 @@ from django.shortcuts import get_object_or_404, render
 from itembank.models import Item
 from assessment.models import TesteeAssessment
 
+# REBUILD AS CBV
 def index(request):
-    active_assesments_list = TesteeAssessment.objects.all().exclude(status='passed').exclude(status='failed') 
-    finished_assesments_list = TesteeAssessment.objects.all().exclude(status='not-started').exclude(status='started') 
+    active_assesments_list = TesteeAssessment.objects.all().filter(testee=request.user).exclude(status='passed').exclude(status='failed') 
+    finished_assesments_list = TesteeAssessment.objects.all().filter(testee=request.user).exclude(status='not-started').exclude(status='started') 
     context = {'active_assesments_list': active_assesments_list, 
                'finished_assesments_list': finished_assesments_list,}  
     return render(request, 'assessment/index.html', context)
 
+# REBUILD AS CBV
 def item(request):
     # body_text = "You are at an assessment item page!"
     item = get_object_or_404(Item, pk=1)

3ddb89cc88e4304ce7ecc6c9aaa1915990484761
FarrowLS <peterwfarrow@yahoo.com>
2014-06-04 18:13:18 -0400

    Updated the random selection of items in assessments

--- assessment/views.py
+++ assessment/views.py
@@ -15,38 +15,52 @@ def index(request):
     return render(request, 'assessment/index.html', context)
     # Rebuild as a CBV down the road
 
-def item(request, testeeassessment_id):
-    
+def item(request, testeeassessment_id):    
     current_testee_assessment = get_object_or_404(TesteeAssessment, pk=testeeassessment_id)
-    
     if not current_testee_assessment.testee == request.user:
         raise PermissionDenied
     else:
         unanswered_items = TesteeResponse.objects.all().filter(testeeassessment=testeeassessment_id).filter(option__isnull=True) 
         if len(unanswered_items) > 0:
             item = get_object_or_404(Item, pk=unanswered_items[0].item_id) 
-            testee_response_id = unanswered_items[0].id        
+            testee_response_id = unanswered_items[0].id
+
+
+            # START TESTING
+            # answered_and_unanswered_items = TesteeResponse.objects.all().filter(testeeassessment=testeeassessment_id)
+            # answered_and_unanswered_items_ids = []
+            # for an_item in answered_and_unanswered_items:
+            #     answered_and_unanswered_items_ids.extend([an_item.item_id])
+            # END TESTING
+
+
         else: 
             # Add try/except here
             current_items = Item.objects.all().filter(itembank = current_testee_assessment.assessment.itembank).filter(status='active')
-
             # Make sure item has not been presented before - NEEDS TO BE UPDATED
             item_approved = False            
             answered_and_unanswered_items = TesteeResponse.objects.all().filter(testeeassessment=testeeassessment_id) 
             answered_and_unanswered_items_ids = []
-            for item_id in answered_and_unanswered_items:
-                answered_and_unanswered_items_ids.extend([item_id])
+            for an_item in answered_and_unanswered_items:
+                answered_and_unanswered_items_ids.extend([an_item.item_id])
+
             # More items in assessment than in itembank issue to be delt with later 
-            while item_approved == False:
-                random_item_position = random.randint(0, len(current_items) - 1)         
-                item = current_items[random_item_position]        
-                if not item.id in answered_and_unanswered_items_ids:
-                    item_approved = True  
+            
+            random_item_position = random.randint(0, len(current_items) - 1)         
+            item = current_items[random_item_position]        
+           
+            # Refactor this ugly hack - turn the selection code above into a function  
+            if item.id in answered_and_unanswered_items_ids:
+                return HttpResponseRedirect(reverse('assessmentitem', args=(testeeassessment_id,))) 
+    
             new_testee_response = TesteeResponse(testeeassessment=current_testee_assessment, item=item, option=None) 
             new_testee_response.save()        
             testee_response_id = new_testee_response.id
+
         context = {'item': item,
-                   'testee_response_id': testee_response_id}      
+                   'testee_response_id': testee_response_id,}
+                   # 'testdata1': answered_and_unanswered_items_ids,}      
+
         return render(request, 'assessment/item.html', context)
         # Rebuild as a CBV down the road
         

732c622b692af5beba848f04cdc1f6a1b259e1eb
FarrowLS <peterwfarrow@yahoo.com>
2014-06-10 22:32:37 -0400

    Removed more commented out code

--- assessment/views.py
+++ assessment/views.py
@@ -29,7 +29,7 @@ def item(request, testeeassessment_id):
             current_testee_assessment.save() 
             return HttpResponseRedirect(reverse('assessmentindex',))        
 
-        unanswered_items = TesteeResponse.objects.all().filter(testeeassessment=testeeassessment_id).filter(option__isnull=True) 
+        unanswered_items = TesteeResponse.objects.all().filter(testeeassessment=testeeassessment_id).filter(option__isnull=True)
         if len(unanswered_items) > 0:
             item = get_object_or_404(Item, pk=unanswered_items[0].item_id) 
             testee_response_id = unanswered_items[0].id
@@ -38,7 +38,10 @@ def item(request, testeeassessment_id):
             current_items = Item.objects.all().filter(itembank = current_testee_assessment.assessment.itembank).filter(status='active')
             # Make sure item has not been presented before - NEEDS TO BE UPDATED
             item_approved = False            
+
+            # TEST THE CODE BELOW TO SEE IF IT WILL GET 1 RANDOM ITEM FROM DB
 	    # objects.all().filter(isnull=True).filter('?')[:1]
+
             answered_and_unanswered_items = TesteeResponse.objects.all().filter(testeeassessment=testeeassessment_id) 
             answered_and_unanswered_items_ids = []
             for an_item in answered_and_unanswered_items:
@@ -49,15 +52,14 @@ def item(request, testeeassessment_id):
             random_item_position = random.randint(0, len(current_items) - 1)         
             item = current_items[random_item_position]        
            
-            # Refactor this ugly hack - turn the selection code above into a function  
+            # Refactor this ugly hack - turn the selection code above into a function 
+ 
             if item.id in answered_and_unanswered_items_ids:
  	        # Add django.shortcuts.render here.
                 return HttpResponseRedirect(reverse('assessmentitem', args=(testeeassessment_id,))) 
-    
             new_testee_response = TesteeResponse(testeeassessment=current_testee_assessment, item=item, option=None) 
             new_testee_response.save()        
             testee_response_id = new_testee_response.id
-
         context = {'item': item,
                    'testee_response_id': testee_response_id,}
         return render(request, 'assessment/item.html', context)
@@ -67,32 +69,8 @@ def response(request, testee_response_id):
     try:
         selected_option = Option.objects.get(pk=request.POST['option']) 
     except (KeyError, Option.DoesNotExist):
-
-        # FOR TESTING - TO BE REMOVED
-        """
-        item = get_object_or_404(Item, pk=current_testee_response.item_id)
-        context = {'item': item, 
-                   'error_messge': "Please select an answer."} 
-        return render(request, 'assessment/item.html', context)
-        """
-        # FOR TESTING - TO BE REMOVED
-
         return HttpResponseRedirect(reverse('assessmentitem', args=(current_testee_response.testeeassessment_id,))) 
-
     else:
         current_testee_response.option_id = selected_option.id
-        current_testee_response.save()
-
-        """
-        # MOVING COMPLETION CHECK TO BEGINING OF ITEM DISPLAY ABOVE 
-        number_of_completed_items = TesteeResponse.objects.all().filter(testeeassessment=current_testee_response.testeeassessment).exclude(option__isnull=True)
-        current_assessment = get_object_or_404(Assessment, current_testee_response.testeeassessment.assessment_id)
-        """
-
-        # if current_assessment.itemsneeded >= len(number_of_completed_items):
-        #     return HttpResponseRedirect(reverse('assessmentindex'))        
-
-        # REMOVE THIS NOTE - CHECK TO SEE IF ASSESSMENT IS FINISHED 
-
-        # return HttpResponse(current_assessment.itemsneeded)
+        current_testee_response.save()       
         return HttpResponseRedirect(reverse('assessmentitem', args=(current_testee_response.testeeassessment_id,)))

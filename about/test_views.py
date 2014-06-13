9332a399ee382c9ccb183c04d03d59032cb0c1b4
FarrowLS <peterwfarrow@yahoo.com>
2014-06-13 17:21:18 -0400

    Added setUp to test_views in about app

--- about/test_views.py
+++ about/test_views.py
@@ -5,13 +5,15 @@ from django.contrib.auth.models import User
 
 from about.models import Page
 
-def create_page(title, body):
-    """
-    Creates a pages with a title and body
-    """
-    return Page.objects.create(title=title, body=body)
-
 class AboutIndexTests(TestCase):
+
+    def setUp(self):
+        Page.objects.create(title="Test1 Title", body="Test1 body.")
+        Page.objects.create(title="Test2 Title", body="Test2 body.")
+
+    def tearDown(self):
+        pass   
+
     def test_index_exists(self):
         """
         The About index page should be at /about
@@ -30,9 +32,6 @@ class AboutIndexTests(TestCase):
         """
         The About index page should show a list of About pages
         """
-        test_page1 = create_page(title="Test1 Title", body="Test1 body.")
-        test_page2 = create_page(title="Test2 Title", body="Test2 body.")
-
         response = self.client.get(reverse('aboutindex'), **{'wsgi.url_scheme': 'https'})
         self.assertQuerysetEqual(
             response.context['text2'],
@@ -41,25 +40,25 @@ class AboutIndexTests(TestCase):
 
 
 class AboutDetailTests(TestCase):
-    def test_detail_page_not_accessable(self):
+
+    def setUp(self):
+        self.test_page = Page.objects.create(title="Test Title", body="This is a test body.") # create_page(title="Test Title", body="This is a test body.")
+
+    def tearDown(self):
+       pass   
+
+    def test_detail_page_is_accessable(self):
         """
         An About detail page should be accessable to an anonymous user
         """
-        test_page = create_page(title="Secret Title", body="This is a secret body.") 
-        response = self.client.get(reverse('aboutdetail', args=(test_page.id,)), **{'wsgi.url_scheme': 'https'})
+        response = self.client.get(reverse('aboutdetail', args=(self.test_page.id,)), **{'wsgi.url_scheme': 'https'})
         self.assertEqual(response.status_code, 200)
 
     def test_detail_page_has_text(self):
         """
         An About detail page should have title and body text on it
         """
-        test_page = create_page(title="Test Title", body="This is a test body.")
-        test_user_setup = User.objects.create_user(username='bob', password='secret')
-        test_user = Client()
-        test_user.login(username='bob', password='secret')
-        # Don't think I need this # response = self.client.get(reverse('aboutdetail'))
-        # response = self.client.get(reverse('aboutdetail', args=(test_page.id,)))
-        response = test_user.get(reverse('aboutdetail', args=(test_page.id,)), **{'wsgi.url_scheme': 'https'})
+        response = self.client.get(reverse('aboutdetail', args=(self.test_page.id,)), **{'wsgi.url_scheme': 'https'})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Title")
         self.assertContains(response, "This is a test body.")

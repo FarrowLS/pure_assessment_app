35fcd5efb2115a419eb6c658fdf7efb14f152154
FarrowLS <peterwfarrow@yahoo.com>
2014-06-25 17:52:45 -0400

    Added assessment model tests for status_update()

--- assessment/test_models.py
+++ assessment/test_models.py
@@ -9,7 +9,7 @@ class TesteeAssessmentMethodtests(TestCase):
 
     def setUp(self):
         self.test_itembank1 = Itembank.objects.create(name="Itembank1")
-        self.test_assessment1 = Assessment.objects.create(name="Test1", itembank=self.test_itembank1)
+        self.test_assessment1 = Assessment.objects.create(name="Test1", itembank=self.test_itembank1, itemsneeded=2, itemsneededtopass=1)
         # Setup for items and their options
         self.test_item1 = Item.objects.create(itembank=self.test_itembank1, stem_text="Test stem text1")
         self.test_option1_1 = Option.objects.create(item=self.test_item1, option_text="True", correct_answer=True)
@@ -25,14 +25,41 @@ class TesteeAssessmentMethodtests(TestCase):
 
     def tearDown(self):
         pass
+   
+
+    def test_status_update_returns_not_started(self):
+        """
+        A unstarted assessment should show as 'not-started'  
+        """   
+        self.assertEqual(self.test_userassessment1.status, 'not-started') 
+
 
     def test_status_update_returns_started(self):
         """
         status_update() should return 'started' when an assessment is begun but not finished 
         """
-        pass
+        current_status = self.test_userassessment1.status_update()
+        self.assertEqual(current_status, 'started')
         
+    def test_status_update_returns_failed(self):
+        """
+        status_update() should return 'failed' when an assessment is finished and not passed 
+        """
 
-# Write tests for status_update()
+        TesteeResponse.objects.create(testeeassessment=self.test_userassessment1, item=self.test_item1, option=self.test_option1_2)
+        TesteeResponse.objects.create(testeeassessment=self.test_userassessment1, item=self.test_item2, option=self.test_option2_2)
+
+        current_status = self.test_userassessment1.status_update()
+        self.assertEqual(current_status, 'failed')
+
+    def test_status_update_returns_passed(self):
+        """
+        status_update() should return 'passed' when an assessment is finished and passed 
+        """
+        TesteeResponse.objects.create(testeeassessment=self.test_userassessment1, item=self.test_item1, option=self.test_option1_1)
+        TesteeResponse.objects.create(testeeassessment=self.test_userassessment1, item=self.test_item2, option=self.test_option2_2)
+        current_status = self.test_userassessment1.status_update()
+        self.assertEqual(current_status, 'passed')
+
 
 # Write tests for select_item()

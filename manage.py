3cf1257c9c1e1d6d4b694b6b9ef497752457f2ab
FarrowLS <peterwfarrow@yahoo.com>
2014-07-14 13:10:49 -0400

    Added note about how setting files are located

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+    # NOTE: ALL SETTINGS FILES ARE HANDLED VIA ENVIRONMENTAL VARIABLES. EX. POINTING TO development.py FOR DEV AND production.py FOR PRODUCTION
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pure_assessment_app.settings")
 
     from django.core.management import execute_from_command_line
